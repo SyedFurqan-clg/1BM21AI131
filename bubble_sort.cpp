39ce918de542246b692cf548831eccc06f7b62ae
SyedFurqan-clg <141044762+SyedFurqan-clg@users.noreply.github.com>
2023-08-03 20:20:37 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,32 +1,43 @@
 // Bubble sort in C++
 
+// Optimized bubble sort in C++
+
 #include <iostream>
 using namespace std;
 
-
+// perform bubble sort
 void bubbleSort(int array[], int size) {
-
+    
   // loop to access each array element
-  for (int step = 0; step < size; ++step) {
+  for (int step = 0; step < (size-1); ++step) {
       
-    // loop to compare array elements
-    for (int i = 0; i < size - step; ++i) {
-
-      // compare two adjacent elements
-     
+    // check if swapping occurs
+    int swapped = 0;
+    
+    // loop to compare two elements
+    for (int i = 0; i < (size-step-1); ++i) {
+        
+      // compare two array elements
+      // change > to < to sort in descending order
       if (array[i] > array[i + 1]) {
 
-        // swapping elements if elements
-        // are not in the intended order
+      
         int temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
+        
+        swapped = 1;
       }
     }
+
+    // no swapping means the array is already sorted
+    // so no need of further comparison
+    if (swapped == 0)
+      break;
   }
 }
 
-// print array
+// print an array
 void printArray(int array[], int size) {
   for (int i = 0; i < size; ++i) {
     cout << "  " << array[i];
@@ -37,11 +48,11 @@ void printArray(int array[], int size) {
 int main() {
   int data[] = {-2, 45, 0, 11, -9};
   
-  // find array's length
+  // find the array's length
   int size = sizeof(data) / sizeof(data[0]);
   
   bubbleSort(data, size);
   
-  cout << "Sorted Array in Ascending Order:\n";  
+  cout << "Sorted Array in Ascending Order:\n";
   printArray(data, size);
 }
